46b8d188a85ed7bdc67109862bbec766e67ab7f0
xhxingxing <234231706@qq.com>
2014-11-14 15:53:43 +0800

    update

--- portal/forms.py
+++ portal/forms.py
@@ -7,10 +7,22 @@ from portal.models import News, Friends,Student,Studentworks,Teacher
 class NewsForm(forms.ModelForm):
 
     content = forms.CharField(label='aaa',widget=forms.Textarea(attrs={'class':'vLargeTextField'}))
+    
+    def clean_show_home_page(self):
+        
+        data = self.cleaned_data['show_home_page']
+        print data
+        count=News.objects.filter(show_home_page__exact='1').count()
+        print count
+        raise forms.ValidationError("show_home_page er")
 
+        # Always return the cleaned data, whether you have changed it or
+        # not.
+        return data
+    
     class Meta:
         model = News
-        fields = ('title','publishtime','picture','bigpic','content')
+        fields = ('title','publishtime','picture','bigpic','content','show_home_page')
 
 #    def __init__(self, *args, **kwargs):
 #
