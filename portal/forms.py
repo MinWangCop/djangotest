e2ab9b01d1c23b68e0da4f47f4bee9aa1b34ad0a
xhxingxing <234231706@qq.com>
2014-11-18 10:07:24 +0800

    update

--- portal/forms.py
+++ portal/forms.py
@@ -1,7 +1,10 @@
+# -*- coding:UTF-8 -*-
 from django import forms
 from django.contrib import admin
 from django.core.exceptions import ValidationError
 from portal.models import News, Friends,Student,Studentworks,Teacher
+from django.forms.fields import FileField  
+
 
 
 class NewsForm(forms.ModelForm):
@@ -19,6 +22,34 @@ class NewsForm(forms.ModelForm):
         # Always return the cleaned data, whether you have changed it or
         # not.
         return data
+    def clean(self):
+        print self
+        
+        s=self.data
+        self.cleaned_data = {}
+                # �������һ��for �Ǵ� django��forms.py�е� full_clean �и���4��  
+        for name, field in self.fields.items():  
+            # value_from_datadict() gets the data from the data dictionaries.  
+            # Each widget type knows how to retrieve its own data, because some  
+            # widgets split data over several HTML fields.  
+            value = field.widget.value_from_datadict(self.data, self.files, self.add_prefix(name))  
+            try:  
+                if isinstance(field, FileField):  
+                    initial = self.initial.get(name, field.initial)  
+                    value = field.clean(value, initial)  
+                else:  
+                    value = field.clean(value)  
+                self.cleaned_data[name] = value  
+                if hasattr(self, 'clean_%s' % name):  
+                    value = getattr(self, 'clean_%s' % name)()  
+                    self.cleaned_data[name] = value  
+            except ValidationError, e:  
+                self._errors[name] = self.error_class(e.messages)  
+                if name in self.cleaned_data:  
+                    del self.cleaned_data[name]  
+        #cl=self.cleaned_data  
+        #debug()<<<�����õ�,�鿴cl��ֵ,��Ҫ�ǿ�self.cleaned_data��ֵ,���return��,�Ϳ�������  
+        return self.cleaned_data  
     
     class Meta:
         model = News
